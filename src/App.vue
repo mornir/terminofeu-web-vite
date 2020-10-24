f393dcd53b16c21cd77ad72ff322cdd6376b6313
mornir <jeromepott@hotmail.com>
2020-10-24 11:36:54 +0200

    Add sanity client

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,25 @@
 </template>
 
 <script>
+import PicoSanity from "picosanity"
 import TheHeader from "./components/SingleInstance/TheHeader.vue"
+
+const client = new PicoSanity({
+  projectId: "nipfx4rq",
+  dataset: "production",
+  useCdn: true,
+})
 
 export default {
   name: "App",
   components: {
     TheHeader,
+  },
+  created() {
+    client
+      .fetch(`*[_type == "entry"]`)
+      .then((entries) => console.log(entries))
+      .catch((err) => console.error("Oh noes: %s", err.message))
   },
 }
 </script>
