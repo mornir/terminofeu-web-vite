695f0717e5150c5641cbe899d68dfc6090a1a836
mornir <jeromepott@hotmail.com>
2020-10-24 09:56:21 +0200

    Add Tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot replacement.</p>
+  <p class="text-xl text-red-500">
+    Edit <code>components/HelloWorld.vue</code> to test hot replacement.
+  </p>
 </template>
 
 <script>
