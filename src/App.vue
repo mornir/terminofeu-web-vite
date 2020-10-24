50a7e75fdc0bb18a99350e9d176c81a9a15cc629
mornir <jeromepott@hotmail.com>
2020-10-24 11:02:47 +0200

    Add language switcher

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <TheHeader />
+  <p>{{ $t("hello") }}</p>
 </template>
 
 <script>
--- src/components/SingleInstance/TheHeader.vue
+++ src/components/SingleInstance/TheHeader.vue
@@ -1,5 +1,17 @@
 <template>
   <header>
-    <nav>{{ $t("hello") }}</nav>
+    <nav class="flex justify-evenly">
+      <button @click="$i18n.locale = 'de'">Deutsch</button>
+      <button @click="$i18n.locale = 'fr'">Français</button>
+      <button>Italieno</button>
+    </nav>
   </header>
 </template>
+
+<script>
+export default {
+  methods: {
+    name() {},
+  },
+}
+</script>
